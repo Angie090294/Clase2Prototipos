0ed800276da4b90c30fcd05ed1c0e5a6cce6bd2e
Alumno-J <Alumno-J>
2016-03-29 21:48:42 -0500

    PersonajeJump

---
+++ Clase2/Assets/Scripts/BgLooper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgLooper : MonoBehaviour {
+
+	// Use this for initialization
+	Material mat;
+	public float Speed;
+	Vector2 offset;
+
+	void Start () {
+		mat = GetComponent<Renderer> ().material;
+		offset = mat.GetTextureOffset ("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset.x += Speed * Time.deltaTime;
+		mat.SetTextureOffset ("_MainTex", offset);
+	}
+}
---
+++ Clase2/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerJump : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D body;
+	bool canJump;
+	Button jumpButton;
+	float jumpPower = 10f;
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+		jumpButton = GameObject.Find ("btnTouch").GetComponent<Button> ();
+		jumpButton.onClick.AddListener(() => jump());
+		canJump = false;
+	}
+
+	void jump(){
+		if (canJump) {
+			canJump = false;
+			body.velocity = new Vector2 (body.velocity.x, jumpPower);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs (body.velocity.y) == 0) {
+			canJump = true;
+		}
+	}
+}
